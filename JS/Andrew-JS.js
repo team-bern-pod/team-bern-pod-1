63986ac6789f89ca833337bdb5d43ee182cc79f8
Andrew Ball <andy.bob.ball@gmail.com>
2020-02-16 11:08:46 -0600

    sunday

--- JS/Andrew-JS.js
+++ JS/Andrew-JS.js
@@ -5,59 +5,52 @@
 // Make a function called twelveSidedDie() that returns a random integer between 1 and 12.
 // Make a function called tetrahedron() that returns a random integer between 1 and 4.
 // Make a function called rollDie() that returns an integer between 1 and 6.
-
-
-
 function randomIntBetween(input) {
     return Math.floor(Math.random() * 2);
 }
-
 // console.log(randomIntBetween());
-
-
 function twoDice(input) {
     return (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
 }
-
 // console.log(twoDice());
-
-
 function twentySidedDice(input) {
     return (Math.floor(Math.random() * 20) + 1);
 }
-
 // console.log(twentySidedDice());
-
-
 function twelveSidedDie(input) {
     return (Math.floor(Math.random() * 12) + 1);
 }
-
-
-
-function tetrahedron(input){
+function tetrahedron(input) {
     return (Math.floor(Math.random() * 4) + 1);
 }
-
 // console.log(tetrahedron());
-
-
 function rollDie(n) {
     return (Math.floor(Math.random() * 6) + 1);
 }
 // console.log(rollDie());
 // Make a function called listOfRollsFromDieFunc(numberOfRolls, diceFunction)
-
 function listOfRollsFromDieFunc(numberOfRolls, input) {
-        var diceArray = [];
-        for (var i = 0; i < numberOfRolls; i++) {
-                diceArray.push(input);
-                console.log(diceArray[i]);
-        }
-        return diceArray
+    var diceArray = [];
+    for (var i = 0; i < numberOfRolls; i++) {
+        diceArray.push(twelveSidedDie());
+    }
+    return diceArray
 }
-console.log(listOfRollsFromDieFunc(5, twelveSidedDie()));
+// console.log(numberOfRolls(4, rollDie()));
 
+//this is the number of rolls picker
+(function() {
+    var diceArray = [];
+    var numberOfRolls = document.getElementById('numberOfRolls');
+    var diceToRoll = document.getElementById('diceToRoll');
+    for (var i = 0; i < numberOfRolls; i++)
+        return diceArray;
+        diceArray.push(numberOfRolls());
+
+    console.log(numberOfRolls); // prints <button id="numberOfRolls">Click Me</button>
+    console.log(diceToRoll); // prints <button id="diceToRoll">Click Me</button>
+
+})();
 
 
 
@@ -75,11 +68,6 @@ console.log(listOfRollsFromDieFunc(5, twelveSidedDie()));
 
 
 //     For example, if we call listOfDieRollsFromDieFunc(1, tetrahedron), then the function will return an array containing one value that is the result of calling the tetrahedron function.
-
-
-
-
-
 
 
 //1. Make a function called listOfRolls(num)
