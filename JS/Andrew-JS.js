2afa7f79ce834be61fdf7e0c539861a0604a805c
Andrew Ball <andy.bob.ball@gmail.com>
2020-02-19 20:28:15 -0600

    Wednesday Night

--- JS/Andrew-JS.js
+++ JS/Andrew-JS.js
@@ -8,57 +8,79 @@
 function randomIntBetween(input) {
     return Math.floor(Math.random() * 2);
 }
+
 // console.log(randomIntBetween());
-function twoDice(input) {
-    return (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
-}
-// console.log(twoDice());
-function twentySidedDice(input) {
-    return (Math.floor(Math.random() * 20) + 1);
-}
+// function twoDice(input) {
+//     return (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
+// }
+
+// // console.log(twoDice());
+// function twentySidedDice(input) {
+//     return (Math.floor(Math.random() * 20) + 1);
+// }
+
 // console.log(twentySidedDice());
-function twelveSidedDie(input) {
-    return (Math.floor(Math.random() * 12) + 1);
-}
-function tetrahedron(input) {
-    return (Math.floor(Math.random() * 4) + 1);
-}
-// console.log(tetrahedron());
-function rollDie(n) {
-    return (Math.floor(Math.random() * 6) + 1);
-}
+// function twelveSidedDie(input) {
+//     return (Math.floor(Math.random() * 12) + 1);
+// }
+
+// function rollDie(n) {
+//     return (Math.floor(Math.random() * 6) + 1);
+// }
+
 // console.log(rollDie());
+
+// function tetrahedron(input) {
+//     return (Math.floor(Math.random() * 4) + 1);
+// }
+
+// console.log(roller());
 // Make a function called listOfRollsFromDieFunc(numberOfRolls, diceFunction)
-function listOfRollsFromDieFunc(numberOfRolls, input) {
+
+
+// var numberOfRolls = document.getElementById("numberOfRolls").selectedIndex;
+
+// var diceToRoll = document.getElementById("diceToRoll").options;
+// alert("Index: " + numberOfRolls[diceToRoll].index + " is " + numberOfRolls[diceToRoll].text);
+function myFunction() {
+    var x = document.getElementById("mySelect").selectedIndex = '';
+    var y = document.getElementById("mySelect").options;
+    console.log(y, x);
+    if (x[y] === 0) {
+        var x = function rollDie(n) {
+            return (Math.floor(Math.random() * 6) + 1);
+        };
+    } else if (x[y] === 1){
+        var x = function tetrahedron(input) {
+            return (Math.floor(Math.random() * 4) + 1);
+        }
+    }else if (x[y] === 2){
+        var x = function twentySidedDice(input) {
+            return (Math.floor(Math.random() * 20) + 1);
+        }
+    }else if (x[y] === 3){
+        var x = function twoDice(input) {
+            return (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
+        }
+    }
+}
+
+function myFunction2() {
+    var x = document.getElementById("mySelect2").selectedIndex;
+    var y = document.getElementById("mySelect2").options;
+    alert(y[x].text);
+}
+
+function listOfRollsFromDieFunc(numberOfRolls, diceFunction) {
     var diceArray = [];
     for (var i = 0; i < numberOfRolls; i++) {
-        diceArray.push(twelveSidedDie());
+        diceArray.push(diceFunction());
     }
+    console.log(diceArray);
     return diceArray
 }
-// console.log(numberOfRolls(4, rollDie()));
+console.log(listOfRollsFromDieFunc(x, myFunction2));
 
-//this is the number of rolls picker
-(function() {
-    var diceArray = [];
-    var numberOfRolls = document.getElementById('numberOfRolls');
-    var diceToRoll = document.getElementById('diceToRoll');
-    for (var i = 0; i < numberOfRolls; i++)
-        return diceArray;
-        diceArray.push(numberOfRolls());
-
-    console.log(numberOfRolls); // prints <button id="numberOfRolls">Click Me</button>
-    console.log(diceToRoll); // prints <button id="diceToRoll">Click Me</button>
-
-})();
-
-
-
-
-
-// kamea.colors.forEach(function (color) {
-//     console.log("Your Dice roll is" +   )
-// });
 
 // This function should take in two arguments:
 
